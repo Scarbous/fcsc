b50655212894730ed35c47783f2d1c8505b7b96a
Sascha Heilmeier <sheilmeier@gmail.com>
2014-10-30 20:45:22 +0100

    next Clean Up

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-#\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', '# Setting ' . $_EXTKEY . ' plugin TypoScript' . $addLine . '', 43);
-
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fcsc/Configuration/TSconfig/pageTSconfig.txt">');
 
