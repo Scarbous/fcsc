5cef4c5d35c4f56c01f151505ca59c9fbe404877
Sascha Heilmeier <sheilmeier@gmail.com>
2014-11-04 00:36:47 +0100

    optimize table and header Template

--- Classes/ViewHelpers/GetcsvViewHelper.php
+++ Classes/ViewHelpers/GetcsvViewHelper.php
@@ -7,10 +7,11 @@ class GetcsvViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelp
      * @param string $csv
 	 * @param integer $delimiter
      * @param string $quotedInput
+     * @param string $as
      * 
      * @return array
      */
-    public function render($csv, $delimiter = 124, $quotedInput = NULL) {
+    public function render($csv, $delimiter = 124, $quotedInput = NULL, $as = NULL) {
     	$rows = GeneralUtility::trimExplode(LF, $csv);
 		$csv = array();
 		$delimiter = chr(intval($delimiter));
@@ -18,6 +19,20 @@ class GetcsvViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelp
 		foreach($rows as $row) :
 			$csv[] = str_getcsv($row, $delimiter, $quotedInput);
 		endforeach;
-		return $csv;
+
+        if (NULL !== $as) {
+            if ($this->templateVariableContainer->exists($as)) {
+                $backupVariable = $this->templateVariableContainer->get($as);
+                $this->templateVariableContainer->remove($as);
+            }
+            $this->templateVariableContainer->add($as, $csv);
+            $content = $this->renderChildren();
+            $this->templateVariableContainer->remove($as);
+            if (TRUE === isset($backupVariable)) {
+                $this->templateVariableContainer->add($as, $backupVariable);
+            }
+            return $content;
+        }
+        return $csv;
     }
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,4 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fcsc/Configuration/TSconfig/pageTSconfig.txt">');
 
 $TYPO3_CONF_VARS[FE][contentRenderingTemplates][] = 'fcsc/Configuration/TypoScript/';
+$TYPO3_CONF_VARS[FE][activateContentAdapter] = 0;
